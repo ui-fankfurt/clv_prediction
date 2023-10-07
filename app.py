cc0b06c014f59e4a875279ff352ce28531a311a3
ui-fankfurt <ui.frankfurt@gmail.com>
2023-10-07 17:25:01 +0800

    created a simple web app but still getting errors

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
 import numpy as np
-import pandas as pd
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
 import pickle
+from werkzeug.utils import secure_filename
+from datetime import datetime
+import os
+from model import process_csv
 
-app = Flask(__name__)
 
-model = pickle.load(open("model.pkl", 'rb'))
+ALLOWED_EXTENSIONS = set(['csv'])
+# Makes sure the file uploaded is a csv file
+def allowed_filenames(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/predict", methods = ["POST"])
-def predict():
-    json_ = request.json
-    query_df = pd.DataFrame(json_)
-    prediction = model.predict(query_df)
-    return jsonify({"Prediction":list(prediction)})
 
-if __name__ == "__main__":
-    app.run(debug = True)
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/upload', methods = ['GET', "POST"])
+    def upload():
+        if request.method == 'POST':
+            file = request.files['file']
+            if file and allowed_filenames(file.filename):
+                filename = secure_filename(file.filename)
+                #new_filename = f"{filename.split('.')[0]}_{str(datetime.now())}.csv"
+                new_filename = "online_retail_input.csv"
+                save_location = f"input\{new_filename}"#os.path.join('input', new_filename)
+                file.save(save_location)
+                
+                output_file = process_csv(save_location)
+
+                return send_from_directory('output', output_file)
+
+
+            return 'uploaded'
+        return render_template('upload.html')
+    
+    return app
