3cf6f5fa9522b4bfa583b71e8ae59b8ce29fd3cb
ui-fankfurt <ui.frankfurt@gmail.com>
2023-10-10 20:06:50 +0800

    added model performance graph

--- model.py
+++ model.py
@@ -7,13 +7,46 @@ from lifetimes import BetaGeoFitter
 from lifetimes import GammaGammaFitter
 from lifetimes.utils import calibration_and_holdout_data
 from lifetimes.utils import summary_data_from_transaction_data
-from lifetimes.plotting import plot_calibration_purchases_vs_holdout_purchases
 from decimal import Decimal
 import datetime as dt
 import pickle
 import csv
 import datetime
 import seaborn as sns
+
+
+def plot_calibration_purchases_vs_holdout_purchases(model, calibration_holdout_matrix, kind="frequency_cal", n=7, **kwargs):
+
+    x_labels = {
+        "frequency_cal": "Purchases in calibration period",
+        "recency_cal": "Age of customer at last purchase",
+        "T_cal": "Age of customer at the end of calibration period",
+        "time_since_last_purchase": "Time since user made last purchase",
+    }
+    summary = calibration_holdout_matrix.copy()
+    duration_holdout = summary.iloc[0]["duration_holdout"]
+
+    summary["model_predictions"] = model.conditional_expected_number_of_purchases_up_to_time(
+            duration_holdout, summary["frequency_cal"], summary["recency_cal"], summary["T_cal"])
+
+    if kind == "time_since_last_purchase":
+        summary["time_since_last_purchase"] = summary["T_cal"] - summary["recency_cal"]
+        ax = (
+            summary.groupby(["time_since_last_purchase"])[["frequency_holdout", "model_predictions"]]
+            .mean()
+            .iloc[:n]
+            .plot(**kwargs)
+        )
+    else:
+        ax = summary.groupby(kind)[["frequency_holdout", "model_predictions"]].mean().iloc[:n].plot(**kwargs)
+
+    plt.title("Actual Purchases in Holdout Period vs Predicted Purchases")
+    plt.xlabel(x_labels[kind])
+    plt.ylabel("Average of Purchases in Holdout Period")
+    plt.legend()
+    plt.savefig('static/model_validation.png')
+
+    return ax
 
 def process_csv(filename, month):
     ##################################################################
@@ -89,6 +122,8 @@ def process_csv(filename, month):
     model.fit(df_rfmt_cal['frequency_cal'],
             df_rfmt_cal['recency_cal'],
             df_rfmt_cal['T_cal'])
+
+    plot_calibration_purchases_vs_holdout_purchases(model, df_rfmt_cal)
 
     df_rfmt['predicted_purchases'] = model.conditional_expected_number_of_purchases_up_to_time(180,
                                                                                         df_rfmt['frequency'],
